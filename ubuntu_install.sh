c01b34fa96ed77307e2e2d87bd618f55873e66bb
Mohd Abuzar <venomdemon718@gmail.com>
2026-04-28 15:41:56 +0530

    Update ubuntu_install.sh

--- ubuntu_install.sh
+++ ubuntu_install.sh
@@ -3,17 +3,16 @@ clear
 echo -e "\e[1;34m##################################################\e[0m"
 echo -e "\e[1;32m       WELCOME TO THEVOIDKERNEL UBUNTU SETUP      \e[0m"
 echo -e "\e[1;34m##################################################\e[0m"
+
 echo "Updating system and installing dependencies..."
-termux-setup-storage
+# The following flags prevent the 'conffile' prompt error you received
+export DEBIAN_FRONTEND=noninteractive
+pkg update -y
+pkg upgrade -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"
+pkg install termux-x11-nightly -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"
 
-pkg update && pkg upgrade -y
-
+echo "Starting Udroid Installer..."
 . <(curl -Ls https://bit.ly/udroid-installer)
-
 udroid install jammy:xfce4
-
-pkg install x11-repo -y
-
-pkg install termux-x11-nightly -y
 
 echo -e "\e[1;32mInstallation Complete by @thevoidkernel\e[0m"
