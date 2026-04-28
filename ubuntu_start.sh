a49815b4566d19961f1308261cf38dfef9d8e148
Mohd Abuzar <venomdemon718@gmail.com>
2026-04-28 15:52:56 +0530

    Update ubuntu_start.sh

--- ubuntu_start.sh
+++ ubuntu_start.sh
@@ -1,10 +1,14 @@
 #!/data/data/com.termux/files/usr/bin/bash
 clear
-echo -e "\e[1;34m##################################################\e[0m"
-echo -e "\e[1;32m         THEVOIDKERNEL UBUNTU RUNNING             \e[0m"
-echo -e "\e[1;34m##################################################\e[0m"
+echo -e "\e[1;35m[thevoidkernel]\e[0m Starting X11 Server..."
+
+# Start Termux-X11 in the background
+termux-x11 :1 -ac & 
+
+# Wait a moment for the X11 server to initialize
+sleep 2
+
 echo -e "\e[1;35m[thevoidkernel]\e[0m Launching Ubuntu XFCE4..."
-termux-x11 :1  -ac &
-udroid login jammy:xfce4
-export DISPLAY=:1
-startxfce4 &
+
+# Login and execute the desktop environment inside the container
+udroid login jammy:xfce4 -- "export DISPLAY=:1 && startxfce4"
