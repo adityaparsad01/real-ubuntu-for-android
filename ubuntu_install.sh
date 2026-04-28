49ad1ff4e0ce57bf10cf02d0cde16c115fd1ab18
Mohd Abuzar <venomdemon718@gmail.com>
2026-04-28 15:34:59 +0530

    Update ubuntu_install.sh

--- ubuntu_install.sh
+++ ubuntu_install.sh
@@ -4,9 +4,16 @@ echo -e "\e[1;34m##################################################\e[0m"
 echo -e "\e[1;32m       WELCOME TO THEVOIDKERNEL UBUNTU SETUP      \e[0m"
 echo -e "\e[1;34m##################################################\e[0m"
 echo "Updating system and installing dependencies..."
+termux-setup-storage
+
 pkg update && pkg upgrade -y
+
 . <(curl -Ls https://bit.ly/udroid-installer)
+
 udroid install jammy:xfce4
+
 pkg install x11-repo -y
+
 pkg install termux-x11-nightly -y
+
 echo -e "\e[1;32mInstallation Complete by @thevoidkernel\e[0m"
